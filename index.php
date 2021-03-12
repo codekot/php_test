df92b2f33f9c9304d83ddbc5ff6921c31cff6b8a
codekot <boobookish@gmail.com>
2021-03-12 15:07:40 +0400

    Add post

--- index.php
+++ index.php
@@ -14,35 +14,51 @@ class User {
 	{
 		this->password = $password;
 	}
-} ?>
- -->
+} ?> -->
+
 
 <?php
 
-$username = 'admin';
-$test ='test';
-$password = 'admin';
+$username = $password = $error_message = $success = "";
 
+$user=null;
 
-$error_message=null;
-$user='Sean';
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["username"]) || empty($_POST["password"])){
+    $error_message = "Enter username and password";
+  } else {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $success = "success";
+  }
+  
+};
 
-$pdo = new PDO(
+function getUser($username){
+  $pdo = new PDO(
   'mysql:host=localhost;port=3306;dbname=php_test','root','');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$stmt =$pdo->prepare('SELECT * FROM users WHERE username=:username');
-$stmt->execute([':username'=>$test]);
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt =$pdo->prepare('SELECT * FROM users WHERE username=:username');
+  $stmt->execute([':username'=>$username]);
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  return $result;
+}
 // echo var_dump($result);
-if ($result){
-  $user = $result[0]['username'];
-  $psswrd =  $result[0]['password'];
-}
-else {
-  $error_message = "User not found";
-}
 
- ?>
+
+// if ($result){
+//   $user = $result[0]['username'];
+//   $psswrd =  $result[0]['password'];
+// }
+// else {
+//   $error_message = "User not found";
+// }
+// if($username=="ZZ")
+// {
+//   echo "RED ALERT!";
+// }
+
+?>
 
 <!doctype html>
 <html lang="en">
@@ -60,37 +76,36 @@ else {
  <div class="container">
   <?php if($error_message){
     echo '<div class="alert alert-danger" role="alert">'.$error_message.'</div>';
-   };
-   if($user){
-    echo '<div class="alert alert-success" role="alert">Welcome '.$user.'!</div>';
+  };
+  if($success){
+    echo '<div class="alert alert-success" role="alert">Welcome '.$username.'!</div>';
   }
   ?>
 
-  <form>
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
   	<h1>Login</h1>
   	<div class="row g-3 align-items-center">
-    	<div class="col-auto">
-    		<label for="username" class="col-form-label">Username</label>
-  		</div>
-  		<div class="col-auto">
-    		<input type="username" id="username" class="form-control">
-  		</div>
-  		
-	</div>
-    <div class="row g-3 align-items-center">
-    	<div class="col-auto">
-    		<label for="inputPassword6" class="col-form-label">Password</label>
-  		</div>
-  		<div class="col-auto">
-    		<input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
-  		</div>
-  		
-	</div>
-  </form>
- </div>
+     <div class="col-auto">
+      <label for="username" class="col-form-label">Username</label>
+    </div>
+    <div class="col-auto">
+      <input type="username" name="username" class="form-control">
+    </div>
 
+  </div>
+  <div class="row g-3 align-items-center">
+   <div class="col-auto">
+    <label for="inputPassword6" class="col-form-label">Password</label>
+  </div>
+  <div class="col-auto">
+    <input type="password" name="password" class="form-control" aria-describedby="passwordHelpInline">
+  </div>
+  <div class="form-group">
+    <input type="submit" class="btn btn-primary" value="Login">
+  </div>
 
-
-    
+</div>
+</form>
+</div>
 </body>
 </html>
