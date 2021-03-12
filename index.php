cdd60bc6d9e25538cb1b703dace04c4531275db5
codekot <boobookish@gmail.com>
2021-03-12 15:48:06 +0400

    Verify user on db

--- index.php
+++ index.php
@@ -1,62 +1,65 @@
-<!-- <? php
-class User {
-	public $id;
-	public $username;
-	private $password;
+<!-- <? phpclass User
+{
+    public $id;
+    public $username;
+    private $password;
 
-	public function __construct($id, $username)
-	{
-		this->id = $id;
-		this->username = $username;
-	}
+    public function __construct($id, $username)
+    {
+        this->id = $id;
+        this->username = $username;
+    }
 
-	public function setPassword($password)
-	{
-		this->password = $password;
-	}
+    public function setPassword($password)
+    {
+        this->password = $password;
+    }
 } ?> -->
 
 
 <?php
-
 $username = $password = $error_message = $success = "";
 
-$user=null;
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["username"]) || empty($_POST["password"])){
-    $error_message = "Enter username and password";
-  } else {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $success = "success";
-  }
-  
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    if (empty($_POST["username"]) || empty($_POST["password"]))
+    {
+        $error_message = "Enter username and password";
+    }
+    else
+    {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        //$success = "success";
+        $user = getUser($username);
+        if (!$user)
+        {
+            $error_message = "User not found";
+        }
+        else
+        {
+            $hash_password = $user[0]["password"];
+            if (password_verify($password, $hash_password))
+            {
+                $success = "success";
+            }
+        }
+
+    }
+
 };
 
-function getUser($username){
-  $pdo = new PDO(
-  'mysql:host=localhost;port=3306;dbname=php_test','root','');
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt =$pdo->prepare('SELECT * FROM users WHERE username=:username');
-  $stmt->execute([':username'=>$username]);
-  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  return $result;
-}
-// echo var_dump($result);
+function getUser($username)
+{
+    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=php_test', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE username=:username');
+    $stmt->execute([':username' => $username]);
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+};
 
-
-// if ($result){
-//   $user = $result[0]['username'];
-//   $psswrd =  $result[0]['password'];
-// }
-// else {
-//   $error_message = "User not found";
-// }
-// if($username=="ZZ")
-// {
-//   echo "RED ALERT!";
-// }
 
 ?>
 
@@ -74,17 +77,19 @@ function getUser($username){
 </head>
 <body>
  <div class="container">
-  <?php if($error_message){
-    echo '<div class="alert alert-danger" role="alert">'.$error_message.'</div>';
-  };
-  if($success){
-    echo '<div class="alert alert-success" role="alert">Welcome '.$username.'!</div>';
-  }
-  ?>
+  <?php if ($error_message)
+{
+    echo '<div class="alert alert-danger" role="alert">' . $error_message . '</div>';
+};
+if ($success)
+{
+    echo '<div class="alert alert-success" role="alert">Welcome ' . $username . '!</div>';
+}
+?>
 
-  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  	<h1>Login</h1>
-  	<div class="row g-3 align-items-center">
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <h1>Login</h1>
+    <div class="row g-3 align-items-center">
      <div class="col-auto">
       <label for="username" class="col-form-label">Username</label>
     </div>
